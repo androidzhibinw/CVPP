6d41dc390b519a0f82f32788f458594797522f5c
zhibinw <android_zhibinw@163.com>
2019-09-10 15:59:40 +0800

    update 002

--- 002.py
+++ 002.py
@@ -7,9 +7,14 @@ imshow(image)
 x = [100,100,40,40]
 y = [20,50,20,50]
 
+
+#plot(x,y)
 plot(x,y, 'r*')
 
-plot(x[:2], y[:2]) #100,100,20,50
+#plot(x,y, 'go-')
+#plot(x,y, 'ks:')
+
+#plot(x[:2], y[:2]) #100,100,20,50
 
 title('PANDA')
 
