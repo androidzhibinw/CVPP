08d8d6c4b6eb5f0b115af11af55313eeeaed5f19
zhibinw <android_zhibinw@163.com>
2019-09-10 15:39:21 +0800

    add plot demo

---
+++ 002.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from pylab import *
+
+image = array(Image.open('panda.jpg'))
+imshow(image)
+
+x = [100,100,40,40]
+y = [20,50,20,50]
+
+plot(x,y, 'r*')
+
+plot(x[:2], y[:2]) #100,100,20,50
+
+title('PANDA')
+
+show()
