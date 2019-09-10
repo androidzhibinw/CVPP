9753dfa93b9293246f4b520e89a8ee8f369fc856
zhibinw <android_zhibinw@163.com>
2019-09-10 14:35:17 +0800

    add first demo

---
+++ 001.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+img = Image.open('panda.jpg')
+print img.format, img.size, img.mode
+img = img.convert('L')
+img = img.rotate(90)
+img.thumbnail((64,64))
+img.show()
+#img.save('001.png')
